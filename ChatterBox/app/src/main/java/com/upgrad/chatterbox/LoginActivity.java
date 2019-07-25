35c7d64ebbaedb75f50e9f2658c9bf7680838b28
Rohit Jain <rohit_j@live.com>
2019-07-25 14:15:07 +0530

    added code of 25 july

---
+++ ChatterBox/app/src/main/java/com/upgrad/chatterbox/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.upgrad.chatterbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ PhoneAuthDemo/app/src/main/java/com/upgrad/phoneauthdemo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.upgrad.phoneauthdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
+
+    private FirebaseAuth phoneauth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Todo
+                //We need to get the phone number
+                // send to the firebase auth
+                //read the otp on next screen
+                // confrim the otp
+            }
+        });
+    }
+}
